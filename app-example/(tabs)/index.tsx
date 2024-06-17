c11b2a28301759ddad13f16d7d9a6078ec5300ae
bryson <br441570@dal.ca>
2024-06-17 14:01:50 -0300

    reset app to enable generic scoreboard development

--- app-example/(tabs)/index.tsx
+++ app-example/(tabs)/index.tsx
@@ -16,11 +16,11 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Score Bored!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">Step 1: NNOOOOO it</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
